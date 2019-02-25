6fbe545c322b372958f7069f94260cabb9f04594
Poutet Paul <paul.poutet@gmail.com>
2019-02-25 16:17:07 +0100

    commit final

---
+++ server/manifest.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const Dotenv = require('dotenv');
+const Confidence = require('confidence');
+const Toys = require('toys');
+
+// Pull .env into process.env
+Dotenv.config({ path: `${__dirname}/.env` });
+
+// Glue manifest as a confidence store
+module.exports = new Confidence.Store({
+    server: {
+        host: 'localhost',
+        port: {
+            $env: 'PORT',
+            $coerce: 'number',
+            $default: 3000
+        },
+        debug: {
+            $filter: { $env: 'NODE_ENV' },
+            $default: {
+                log: ['error'],
+                request: ['error']
+            },
+            production: {
+                request: ['implementation']
+            }
+        }
+    },
+    register: {
+        plugins: [
+            {
+                plugin: '../lib', // Main plugin
+                options: {}
+            },
+            {
+                plugin : './plugins/swagger.js'
+            },
+            {
+                plugin: 'schwifty',
+                options: {
+                    $filter: 'NODE_ENV',
+                    $default: {},
+                    $base: {
+                        migrateOnStart: true,
+                        knex: {
+                            client  : 'pg',
+                            connection      : {
+                                host        : process.env.POSTGRES_HOST || 'localhost',
+                                port        : process.env.POSTGRES_PORT || 5432,
+                                user        : process.env.POSTGRES_USER || 'hapi',
+                                password    : process.env.POSTGRES_PASSWORD || 'hapi',
+                                database    : process.env.POSTGRES_DATABASE || 'unilim'
+                            }
+                        }
+                    },
+                    production: {
+                        migrateOnStart: true
+                    }
+                }
+            },
+            {
+                plugin: {
+                    $filter: { $env: 'NODE_ENV' },
+                    $default: 'hpal-debug',
+                    production: Toys.noop
+                }
+            }
+        ]
+    }
+});
